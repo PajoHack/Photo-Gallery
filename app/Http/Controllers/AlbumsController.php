c9e612e0fc73cf22807b8a27d102741173d40305
Pajo <pajohack@gmail.com>
2018-05-12 10:54:20 +0000

    Photo Upload and photo delete functionality working

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Album;
 
 class AlbumsController extends Controller
 {
     public function index()
     {
-        return view('albums.index');
+        $albums = Album::with('photos')->get();
+
+        return view('albums.index', compact('albums'));
     }
 
     public function create()
@@ -18,6 +21,40 @@ class AlbumsController extends Controller
 
     public function store(Request $request)
     {
-        return;
+        $this->validate($request, [
+           'name' => 'required',
+           'cover_image' => 'image|max:1999'
+        ]);
+
+        // filename with extension
+        $filenameWithExt = $request->file('cover_image')->getClientOriginalName();
+
+        // filename without extension
+        $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+
+        // get extension
+        $extension = $request->file('cover_image')->getClientOriginalExtension();
+
+        // create new filename to store
+        $filenameToStore = $filename . '_' . time() . '.' . $extension;
+
+        // upload image
+        $path = $request->file('cover_image')->storeAs('public/album_covers', $filenameToStore);
+
+        $album = new Album;
+        $album->name = $request->input('name');
+        $album->description = $request->input('description');
+        $album->cover_image = $filenameToStore;
+
+        $album->save();
+
+        return redirect('/albums')->with('success', 'Album Created');
+    }
+
+    public function show($id)
+    {
+        $album = Album::with('photos')->find($id);
+
+        return view('albums.show', compact('album'));
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,25 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <title>PhotoShow</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/css/foundation.css">
-</head>
-<body>
-<div class="row">
-    @yield('content')
-</div>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <title>PhotoShow</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/css/foundation.css">
+    </head>
+
+    <body>
+
+    @include('inc.topbar')
+
+    <br>
+
+        <div class="row">
+
+            @include('inc.messages')
+            @yield('content')
+
+        </div>
+
+    </body>
+
 </html>
+
